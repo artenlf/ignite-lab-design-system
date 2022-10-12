5ec30fe71443300e3b7c14a964adc1ec188b059b
artenlf <felipe.arten@gmail.com>
2022-10-12 09:13:04 -0300

    feat: checkbox component

---
+++ src/components/Checkbox.tsx
@@ -0,0 +1,17 @@
+import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
+import { Check } from "phosphor-react";
+import { clsx } from "clsx";
+
+export interface CheckboxProps {}
+
+export function Checkbox({}: CheckboxProps) {
+  return (
+    <CheckboxPrimitive.Root
+      className={clsx("w-6 h-6 p-[2px] bg-grey-800 rounded")}
+    >
+      <CheckboxPrimitive.Indicator asChild>
+        <Check weight="bold" className="h-5 w-5 text-cyan-500" />
+      </CheckboxPrimitive.Indicator>
+    </CheckboxPrimitive.Root>
+  );
+}
