da273dd24fe8c680283e49b8c4c8694a3293a346
artenlf <felipe.arten@gmail.com>
2022-10-12 23:08:59 -0300

    feat: add props to components

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -2,12 +2,13 @@ import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
 import { Check } from "phosphor-react";
 import { clsx } from "clsx";
 
-export interface CheckboxProps {}
+export interface CheckboxProps extends CheckboxPrimitive.CheckboxProps {}
 
-export function Checkbox({}: CheckboxProps) {
+export function Checkbox(props: CheckboxProps) {
   return (
     <CheckboxPrimitive.Root
       className={clsx("w-6 h-6 p-[2px] bg-grey-800 rounded")}
+      {...props}
     >
       <CheckboxPrimitive.Indicator asChild>
         <Check weight="bold" className="h-5 w-5 text-cyan-500" />
